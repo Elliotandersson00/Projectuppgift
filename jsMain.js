f82d81390e9be9b3dd1763442e6c27eda0f12fa8
emil <emil.tornqvist@iths.se>
2022-02-07 12:15:29 +0100

    livevalid

--- jsMain.js
+++ jsMain.js
@@ -11,3 +11,46 @@
   
 }
 
+
+function validation(){
+  var form = document.getElementById("form");
+  var email = document.getElementById("email").value;
+  var text = document.getElementById("text");
+  var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+  
+  if(email.match(pattern))
+  {
+    form.classList.add("valid");
+    form.classList.remove("invalid");
+   
+    text.innerHTML = "Your Email Adress is valid"
+   document.getElementById("text").style.color = "#009933"
+  }
+  else
+  {
+      
+    form.classList.remove("valid");
+    form.classList.add("invalid");
+   
+    text.innerHTML = "Enter a valid Email"
+    document.getElementById("text").style.color = "#ff0000"
+    
+  }
+  
+} 
+// function ValidateEmail(inputText)
+// {
+// var mailformat = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
+// if(inputText.value.match(mailformat))
+// {
+// alert("Valid email address!");
+// document.form1.text1.focus();
+// return true;
+// }
+// else
+// {
+// alert("You have entered an invalid email address!");
+// document.form1.text1.focus();
+// return false;
+// }
+// }
